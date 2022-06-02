2da7499324172ebc2ef9985242eff90dadf0d0fd
Grace <grace.anderson.au@gmail.com>
2022-06-02 10:26:21 +1000

    genre seed file created

--- seeds/genreData.js
+++ seeds/genreData.js
@@ -2,7 +2,31 @@ const { Genre } = require('../models');
 
 const genreData = [
   {
-    genre_title: Fantasy
+    genre_title: 'Comedy'
+  },
+  {
+    genre_title: 'Fantasy'
+  },
+  {
+    genre_title: 'Graphic Novels'
+  },
+  {
+    genre_title: 'High Brow Literature'
+  },
+  {
+    genre_title: 'Historical'
+  },
+  {
+    genre_title: 'Horror'
+  },
+  {
+    genre_title: 'Mystery'
+  },
+  {
+    genre_title: 'Romance'
+  },
+  {
+    genre_title: 'Sci Fi'
   }
 ];
 
