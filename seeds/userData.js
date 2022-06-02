5f46cf451295d37a5b1706e2a04f5cbee135bc1c
Grace <grace.anderson.au@gmail.com>
2022-06-02 10:47:36 +1000

    created bookData seed file

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,26 +1,31 @@
 const { User } = require('../models');
 
 const userData = [
+  //1
   {
     username: 'Amanda',
     email: 'anunes@ufc.com',
     password: 'password12345'
   },
+  //2
   {
     username: 'Khabib',
     email: 'knurmagomedov@ufc.com',
     password: 'password12345'
   },
+  //3
   {
     username: 'Israel',
     email: 'iadesanya@ufc.com',
     password: 'password12345'
   },
+  //4
   {
     username: 'Valentina',
     email: 'vshevchenko@ufc.com',
     password: 'password12345'
   },
+  //5
   {
     username: 'Tyron',
     email: 'twoodley@ufc.com',
