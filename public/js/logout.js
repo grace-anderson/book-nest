36ef3fe44784b05476b2861c1c94d5d123327f3f
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-04 17:18:29 +1000

    added logout feature; adjusted nav bar on main hbs

---
+++ public/js/logout.js
@@ -0,0 +1,18 @@
+// CURRENTLY NOT IN USE
+
+// LOG USER OUT HANDLER
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Logout failed.');
+  }
+};
+
+// GRAB THE ELEMENT
+document.getElementById('logout').addEventListener('click', logout);
