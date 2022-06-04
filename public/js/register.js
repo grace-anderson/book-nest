04e76b8856a495252eb5180a21fee2de8ad42992
Grace <grace.anderson.au@gmail.com>
2022-06-04 10:23:10 +1000

    added home page with text. Added buttons linking to login page and find a book

--- public/js/register.js
+++ public/js/register.js
@@ -1,33 +1,33 @@
-window.onload = function () {
-  const registerForm = document.querySelector('.register-form');
+// window.onload = function () {
+//   const registerForm = document.querySelector('.register-form');
 
-  if (registerForm) {
+//   if (registerForm) {
 
-      event.preventDefault();
-      const username = document.querySelector('#username').value.trim();
-      const password = document.querySelector('#password').value.trim();
-      const email = document.querySelector('#email').value.trim();
+//       event.preventDefault();
+//       const username = document.querySelector('#username').value.trim();
+//       const password = document.querySelector('#password').value.trim();
+//       const email = document.querySelector('#email').value.trim();
 
-      const errorMsgElement = document.querySelector('.error-msg');
-      let errorMsg = '';
+//       const errorMsgElement = document.querySelector('.error-msg');
+//       let errorMsg = '';
 
-      if (username === '' || password === '' || email === '') {
-        errorMsg = 'Please enter all the required fields.';
-      } else if (!/^[^@ ]+@[^@ ]+\.[^@ \.]{2,}$/.test(email)) {
-        errorMsg = 'Please enter a valid email address.';
-      }
+//       if (username === '' || password === '' || email === '') {
+//         errorMsg = 'Please enter all the required fields.';
+//       } else if (!/^[^@ ]+@[^@ ]+\.[^@ \.]{2,}$/.test(email)) {
+//         errorMsg = 'Please enter a valid email address.';
+//       }
 
-      if (errorMsg) {
-        errorMsgElement.innerHTML = errorMsg;
-        return;
-      } else {
-        errorMsgElement.innerHTML = '';
-      }
+//       if (errorMsg) {
+//         errorMsgElement.innerHTML = errorMsg;
+//         return;
+//       } else {
+//         errorMsgElement.innerHTML = '';
+//       }
 
-      if (username !== '' && password !== '' && email !== '') {
-        // make API Call
-        console.log('all good');
-      }
-    });
-  }
-};
+//       if (username !== '' && password !== '' && email !== '') {
+//         // make API Call
+//         console.log('all good');
+//       }
+//     });
+//   }
+// };
--- views/partials/dummy.js
+++ views/partials/dummy.js
@@ -1,2 +1,4 @@
+//NOTE ****** we have not been able to use partials with prettier *****
+
 // this is a placeholder file.
 // delete at the end of the project when cleaning up files
