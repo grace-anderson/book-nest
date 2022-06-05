3282e4ab0756e738ec19327d05edfd46ac88399c
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-05 20:10:18 +1000

    added more books to seeds; changed users in seeds; added books to seeded user reading list

--- seeds/bookData.js
+++ seeds/bookData.js
@@ -4,6 +4,13 @@ const { Book } = require('../models');
 
 const bookData = [
   {
+    title: '488 Rules for Life',
+    author: 'Kitty Flanagan',
+    publication_year: 2019,
+    user_shared_id: 1,
+    genre_id: 1
+  },
+  {
     title: 'Harry Potter - The Complete Collection',
     author: 'JK Rowling',
     publication_year: 2014,
@@ -11,18 +18,40 @@ const bookData = [
     genre_id: 2
   },
   {
-    title: 'To Paradise',
-    author: 'Hanya Yanagihara',
-    publication_year: 2022,
-    user_shared_id: 4,
-    genre_id: 4
-  },
-  {
     title: 'Watchmen',
     author: 'Alan Moore',
     publication_year: 1986,
-    user_shared_id: 3,
+    user_shared_id: 4,
     genre_id: 3
+  },
+  {
+    title: 'To Paradise',
+    author: 'Hanya Yanagihara',
+    publication_year: 2022,
+    user_shared_id: 3,
+    genre_id: 4
+  },
+
+  {
+    title: 'A Gentleman in Moscow',
+    author: 'Amor Towles',
+    publication_year: 2018,
+    user_shared_id: 2,
+    genre_id: 5
+  },
+  {
+    title: 'It',
+    author: 'Stephen King',
+    publication_year: 1986,
+    user_shared_id: 5,
+    genre_id: 6
+  },
+  {
+    title: 'And Then There Were None',
+    author: 'Agatha Christie',
+    publication_year: 1939,
+    user_shared_id: 3,
+    genre_id: 7
   },
   {
     title: 'The Decline and Fall of the Roman Empire',
@@ -32,11 +61,18 @@ const bookData = [
     genre_id: 8
   },
   {
-    title: '488 Rules for Life',
-    author: 'Kitty Flanagan',
-    publication_year: 2019,
+    title: 'Rough and Ready',
+    author: 'Sandra Hill',
+    publication_year: 2006,
+    user_shared_id: 4,
+    genre_id: 9
+  },
+  {
+    title: 'Dune',
+    author: 'Frank Herbert',
+    publication_year: 1965,
     user_shared_id: 1,
-    genre_id: 1
+    genre_id: 10
   }
 ];
 
--- seeds/bookReadingListData.js
+++ seeds/bookReadingListData.js
@@ -14,8 +14,20 @@ const bookReadingListData = [
     book_id: 5
   },
   {
+    reading_list_id: 1,
+    book_id: 10
+  },
+  {
     reading_list_id: 2,
     book_id: 4
+  },
+  {
+    reading_list_id: 2,
+    book_id: 3
+  },
+  {
+    reading_list_id: 2,
+    book_id: 8
   },
   {
     reading_list_id: 3,
@@ -23,11 +35,15 @@ const bookReadingListData = [
   },
   {
     reading_list_id: 4,
-    book_id: 2
+    book_id: 9
   },
   {
     reading_list_id: 4,
     book_id: 4
+  },
+  {
+    reading_list_id: 4,
+    book_id: 7
   },
   {
     reading_list_id: 5,
@@ -39,7 +55,7 @@ const bookReadingListData = [
   },
   {
     reading_list_id: 5,
-    book_id: 3
+    book_id: 6
   },
   {
     reading_list_id: 5,
@@ -47,7 +63,7 @@ const bookReadingListData = [
   },
   {
     reading_list_id: 5,
-    book_id: 5
+    book_id: 10
   }
 ];
 
