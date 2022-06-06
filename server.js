eb863066ff7fbfe1bf7f12f0f8a103bfc1326fd7
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-07 09:27:22 +1000

    added comments to server.js and small note in bookCard hbs

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 // import dotenv
 require('dotenv').config();
 
+// import express modules
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+
+// import handlebars and hbs helpers
 const exphbs = require('express-handlebars');
-
-const routes = require('./controllers');
-
-// uncomment when helpers have been added (refer to line 21 as well)
 const helpers = require('./utils/helpers');
 
+// import routes
+const routes = require('./controllers');
+
+// establish sequelize
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// when helpers have been added:
-// delete the following line:
-// const hbs = exphbs.create();
-// and then UNCOMMENT the following line:
+// establish helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.COOKIE_SECRET, // change to environment variable
+  secret: process.env.COOKIE_SECRET, // change to environment variable (cookie now private)
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -33,8 +33,10 @@ const sess = {
   })
 };
 
+// create session
 app.use(session(sess));
 
+// use handlebars and hbs helpers
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -42,8 +44,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// use routes
 app.use(routes);
 
+// start the server!
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App now listening on port ${PORT}`));
 });
