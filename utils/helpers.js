18029c4bb6c2aac973b9057a275d385af5e70086
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-07 09:09:13 +1000

    added comment to format_date helper with example date output

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,11 @@
 module.exports = {
   format_date: (date) => {
+    // example date output : 5 Jun 2022
     const options = {
       year: 'numeric',
       month: 'short',
       day: 'numeric'
     };
-
     return date.toLocaleDateString('en-GB', options);
   }
 };
